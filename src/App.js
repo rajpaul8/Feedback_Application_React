a8c0422194e5a1dc492c6ba3fe3a798f407f5e9c
rajpaul8 <rajpaul83@gmail.com>
2022-07-10 18:02:52 +0530

    Added Feedback Statistics Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
   const removeFeedback = (item) => {
-    if(window.confirm('Are you sure you want to delete>')){
+    if (window.confirm("Are you sure you want to delete>")) {
       setFeedback(
         feedback.filter((e) => {
           return e !== item;
@@ -19,6 +21,7 @@ function App() {
     <>
       <Header></Header>
       <div className="container">
+        <FeedbackStats item={feedback}></FeedbackStats>
         <FeedbackList item={feedback} removeFeedback={removeFeedback} />
       </div>
     </>
